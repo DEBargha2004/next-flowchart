9a89de471d57b10deffdee973da08348fcb5c94c
DEBargha2004 <debarghasaha16@gmail.com>
2023-12-23 22:23:46 +0530

    feat
    clerk auth added

---
+++ src/app/signin/[[...signin]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from '@clerk/nextjs'
+
+function Page () {
+  return (
+    <div className='w-full h-full flex justify-center items-center'>
+      <SignIn />
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/signup/[[...signup]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from '@clerk/nextjs'
+
+function Page () {
+  return (
+    <div className='w-full h-full flex justify-center items-center'>
+      <SignUp />
+    </div>
+  )
+}
+
+export default Page
